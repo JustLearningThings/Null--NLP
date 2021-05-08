9f52d6fef844d0d8d786bd251d310a346502764a
Denny06 <denismacio@outlook.com>
2021-05-08 22:36:07 +0300

    updated model max_length properties

--- null_blog/blog/models.py
+++ null_blog/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=64)
-    content = models.TextField(max_length=512)
+    content = models.TextField(max_length=2048)
     image = models.ImageField(upload_to='uploads/post/', blank=True)
     pub_date = models.DateTimeField('date published')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user', default=0)
@@ -15,7 +15,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    content = models.TextField(max_length=256)
+    content = models.TextField(max_length=1024)
     pub_date = models.DateField(auto_now=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='related post', default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user', default=0)
