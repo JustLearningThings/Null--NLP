35950fc5cd9662723db592bea50ca1c8e8abd64d
Denny06 <denismacio@outlook.com>
2021-05-09 17:15:32 +0300

    changed model pickle's path to be relative

--- null_blog/blog/model/nlp_model.py
+++ null_blog/blog/model/nlp_model.py
@@ -4,17 +4,17 @@ from .preprocessing import TextTransformer
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 
-# import os
+import os
 
 # instanciate a transformer for preprocessing text
 transformer = TextTransformer(stopwords=['and', 'for', 'in', 'about', 'for', 'as', 'the'], stemmer=PorterStemmer())
 
 # unpacking the model
-path_to_model = r'D:\NULL++ NLP\null_blog\blog\model\model.pkl'
+path_to_model = os.path.join(os.getcwd(), r'blog\model\model.pkl')
 model = load(path_to_model)
 
 # function used in views to check if text content is offensive
 def is_offensive(text):
     text = transformer.transform([text])
-    print(text)
+
     return model.predict(text)
