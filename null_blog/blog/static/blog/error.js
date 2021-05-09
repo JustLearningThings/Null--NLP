7cb52e9a297612fb99a07844033ef334e81efbc3
Denny06 <denismacio@outlook.com>
2021-05-09 16:56:36 +0300

    modified the error modal

--- null_blog/blog/static/blog/error.js
+++ null_blog/blog/static/blog/error.js
@@ -1,4 +1,6 @@
 let error = document.getElementById('error-wrapper')
 let btn = document.getElementById('error-close-btn')
+let close = document.getElementById('error-close')
 
-btn.onclick = () => error.classList.add('display-none')
+btn.onclick = () => error.classList.add('hidden')
+close.onclick = () => error.classList.add('hidden')
