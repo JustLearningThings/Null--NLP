645f92bf7cfed518632dcbd63401afc3bdcd4661
Denny06 <denismacio@outlook.com>
2021-05-14 20:44:37 +0300

    fixed modal bug

--- null_blog/blog/static/blog/error.js
+++ null_blog/blog/static/blog/error.js
@@ -1,6 +1,14 @@
 let error = document.getElementById('error-wrapper')
 let btn = document.getElementById('error-close-btn')
 let close = document.getElementById('error-close')
+let body = document.querySelector('body')
 
-btn.onclick = () => error.classList.add('hidden')
-close.onclick = () => error.classList.add('hidden')
+body.classList.add('error-body')
+
+let closeModal = () => {
+    error.classList.add('hidden')
+    body.classList.remove('error-body')
+}
+
+btn.onclick = () => closeModal()
+close.onclick = () => closeModal()
